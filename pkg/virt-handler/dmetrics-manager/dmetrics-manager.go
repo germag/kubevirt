b7dd6f315e4a528b1281683d65ef86dedfd36df9
German Maglione <gmaglione@redhat.com>
2023-07-11 11:56:21 +0200

    DownwardMetrics: Add DownwardMetrics servers manager
    DownwardMetricsManager is in charge of starting and stopping each
    DownwardMetrics servers. The lifetime of each server is tied to the VMI
    and the manager itself. The DownwardMetricsManager will watch for VMI
    Update and Delete events in its node, and will launch and stop a
    metrics server for each of VMI.
    
    Signed-off-by: Javier Cano Cano <jcanocan@redhat.com>
    Signed-off-by: German Maglione <gmaglione@redhat.com>

---
+++ pkg/virt-handler/dmetrics-manager/dmetrics-manager.go
@@ -0,0 +1,105 @@
+package dmetrics_manager
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"k8s.io/apimachinery/pkg/types"
+
+	v1 "kubevirt.io/api/core/v1"
+
+	"kubevirt.io/kubevirt/pkg/downwardmetrics"
+	virtioserial "kubevirt.io/kubevirt/pkg/downwardmetrics/virtio-serial"
+	cmdclient "kubevirt.io/kubevirt/pkg/virt-handler/cmd-client"
+)
+
+func NewDownwardMetricsManager(nodeName string) *DownwardMetricsManager {
+	return &DownwardMetricsManager{
+		done:       false,
+		nodeName:   nodeName,
+		stopServer: make(map[types.UID]context.CancelFunc),
+	}
+}
+
+// DownwardMetricsManager controls the lifetime of the DownwardMetrics servers.
+// Each server is tied to the lifetime of the VMI and DownwardMetricsManager itself.
+type DownwardMetricsManager struct {
+	lock       sync.Mutex
+	done       bool
+	nodeName   string
+	stopServer map[types.UID]context.CancelFunc
+}
+
+// Run blocks until stopCh is closed. When done, it stops all remaining
+// running DownwardMetrics servers.
+func (m *DownwardMetricsManager) Run(stopCh chan struct{}) {
+	defer m.stop()
+	<-stopCh
+}
+
+func (m *DownwardMetricsManager) stop() {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	m.done = true
+
+	// Stop all DownwardMetrics servers
+	for vmiUID, stopServerFn := range m.stopServer {
+		stopServerFn()
+		delete(m.stopServer, vmiUID)
+	}
+}
+
+// StopServer removes the VMI name from the list of served VMs a
+// nd stop the DownwardMetrics server if necessary
+func (m *DownwardMetricsManager) StopServer(vmi *v1.VirtualMachineInstance) {
+	if !downwardmetrics.HasDevice(&vmi.Spec) {
+		return
+	}
+
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	if m.done {
+		return
+	}
+
+	// Even it's not strictly required for stopping the server,
+	// since the server will stop when the VM closes the unix socket,
+	// we cancel the context to avoid leaking it
+	m.stopServer[vmi.UID]()
+	delete(m.stopServer, vmi.UID)
+}
+
+// StartServer start a new DownwardMetrics server if the VM request it and is not already started
+func (m *DownwardMetricsManager) StartServer(vmi *v1.VirtualMachineInstance, pid int) error {
+	if !downwardmetrics.HasDevice(&vmi.Spec) || !vmi.IsRunning() {
+		return nil
+	}
+
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	if m.done {
+		return nil
+	}
+
+	if _, alreadyStarted := m.stopServer[vmi.UID]; alreadyStarted {
+		return nil
+	}
+
+	launcherSocketPath, err := cmdclient.FindSocketOnHost(vmi)
+	if err != nil {
+		return fmt.Errorf("failed to get the launcher socket for VMI [%s], error: %v", vmi.GetName(), err)
+	}
+
+	channelPath := downwardmetrics.ChannelSocketPathOnHost(pid)
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	err = virtioserial.RunDownwardMetricsVirtioServer(ctx, m.nodeName, channelPath, launcherSocketPath)
+	if err != nil {
+		cancelCtx()
+		return fmt.Errorf("failed to start the DownwardMetrics stopServer for VMI [%s], error: %v", vmi.GetName(), err)
+	}
+	m.stopServer[vmi.UID] = cancelCtx
+
+	return nil
+}
